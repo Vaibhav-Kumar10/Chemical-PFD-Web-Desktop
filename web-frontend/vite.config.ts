81bc97a9c2fb9b2cd30dcfc70569a5a42a8ef359
Harshit Kandpal <kandpalhar@gmail.com>
2026-01-13 20:46:52 +0530

    Configured paths

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
+import path from "path";
+
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 
   test: {
     environment: "jsdom",
